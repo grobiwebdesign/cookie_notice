91d5bc7624254c926109cafd775bf1e8c792134d
Graeme Robinson <1robig68@solent.ac.uk>
2018-06-11 21:09:35 +0100

    js temporary fix
    temporarily fixed JS hide function as the file wasn't being registered

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//close cookie notice
+
+document.getElementById('accept') 
+    .addEventListener('click', function() {
+    document.querySelector('.cookie_notice').style.display = "none";
+    });
