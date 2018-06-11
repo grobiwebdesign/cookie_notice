b7bdd4b999fac4e222e148bfd643e69a9862b9fb
Graeme Robinson <1robig68@solent.ac.uk>
2018-06-11 20:17:55 +0100

    Removed JS, added CSS

--- js/main.js
+++
@@ -1,9 +0,0 @@
-//cookie notice
-$(document).ready(function(){   
-    setTimeout(function () {
-        $(".cookie_notice").fadeIn(200);
-     }, 4000);
-    $(".accept").click(function() {
-        $(".cookie_notice").fadeOut(200);
-    }); 
-}); 
