55ad6845206966724000e6a95994507d96106c07
Graeme Robinson <1robig68@solent.ac.uk>
2018-06-11 18:00:38 +0100

    Changes to JS
    Should include recognition cookies to keep banner reappearing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 //cookie notice
-if($('.cookie_notice').length){
-    $('.cookie_notice').slideDown(800);
-}
+$(document).ready(function(){   
+    setTimeout(function () {
+        $(".cookie_notice").fadeIn(200);
+     }, 4000);
+    $(".accept").click(function() {
+        $(".cookie_notice").fadeOut(200);
+    }); 
+}); 
